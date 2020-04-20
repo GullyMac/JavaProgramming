a04d451e46c011142db89e7df7638e857294fdf3
kccistc <kccistc@DESKTOP-LIA6VNT>
2020-04-20 11:01:10 +0900

    0420 11:00

--- src/practice8_0420/Test4.java
+++ src/practice8_0420/Test4.java
@@ -1,10 +1,22 @@
 package practice8_0420;
 
+class Box<T> {
+	private T t;
+	public void add(T t) {
+		this.t = t;
+	}
+	public T get( ) {
+		return this.t;
+	}
+}
 public class Test4 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Box<Integer> b1 = new Box<Integer>();
+		b1.add(new Integer(2));
+		Box<String> s1 = new Box<String>();
+		s1.add("Hello Java");
+		System.out.println(b1.get());
+		System.out.println(s1.get());
 	}
 
 }
---
+++ src/practice8_0420/p491.java
@@ -0,0 +1,10 @@
+package practice8_0420;
+
+public class p491 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
